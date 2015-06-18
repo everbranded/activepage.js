ee3107a1c499b8122999b03f9ce947edcc468eea
Zach Alam <zelphics@gmail.com>
2015-06-17 20:33:41 -0700

    checks activepage class

--- activepage.js
+++ activepage.js
@@ -1,6 +1,15 @@
-
 $(document).ready(function() {
+    
+    // get the location of this page
+    var location = window.location.href;
+
+    
+     // check for each instance of active page
+    $(".activepage").each(function() {
+    
+        // get the href attribute of this element.
+        var href = $(this).attr("href");
+          
+    });
   
-  // doc ready
-  
-});
+
