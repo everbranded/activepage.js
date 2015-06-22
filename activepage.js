c3f2cc55ba23c4902e25222f166c2557017d9969
Zach Alam <zelphics@gmail.com>
2015-06-21 22:48:48 -0400

    sets default activepage class

--- activepage.js
+++ activepage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     
     // get the location of this page
-    var location = window.location.href;
+    var location = window.location;
 
     
      // check for each instance of active page
@@ -9,7 +9,17 @@ $(document).ready(function() {
     
         // get the href attribute of this element.
         var href = $(this).attr("href");
+        
+        // get the active class to set this to.
+        var data_class = $(this).attr("data-class");
+        
+        // if no data-class attribute is set
+        // add the default active page class
+        // .activepage-active
+        if(!data_class) data_class = "activepage-active";
+        
+        // check if this is the active page.
+        console.log(location);
           
     });
-  
-
+});
